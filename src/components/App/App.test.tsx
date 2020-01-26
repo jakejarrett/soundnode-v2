12609d89bc7c20a41cdf60723a757864a99d3256
Jake Jarrett <jakeryanjarrett@gmail.com>
2020-01-26 17:08:37 +1100

    Add testing utils

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render } from 'enzyme';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/** TODO: Add tests once MVP is written */
+test('Renders fine', () => {
+  const component = render(<App />);
+  expect(component).toMatchSnapshot();
 });
